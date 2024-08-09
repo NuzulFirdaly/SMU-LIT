1af76bdffb30ea4464a3acf21e8fb31de1c34a10
Chai Pin Zheng <chaipinzheng@gmail.com>
2024-08-09 23:49:20 +0800

    Simple Forced Redirect if user is on sammyboy

---
+++ chrome-extension-webpack-main/src/contentScript.ts
@@ -0,0 +1,4 @@
+// Redirect to google.com if user is found browsing facebook.com and subpages
+if (window.location.href.includes('sammyboy.com')) {
+  window.location.href = 'https://www.google.com';
+}
